68ad7e9566ba83ce5ee8dcf197309472b1e65ac8
Eugene Dryahlov <e.dryahlov@i2013.ru>
2015-10-04 15:45:05 +0400

    ver 0.2

--- snake.js
+++ snake.js
@@ -3,7 +3,7 @@
 window.onload = function() {
     "use strict";
     
-    var version = 0.1;
+    var version = 0.2;
     document.getElementById("ver").innerHTML = '(ver. '+ version+')';
 
     var canvas = document.getElementById("game");
@@ -13,12 +13,19 @@ window.onload = function() {
     var blockSpacer = 2;
     var fieldSize = 40;
     var snakeLength = 5;
+    var rabbitsNumber = 20;
+    
+    var snakeCoordsX = [], snakeCoordsY = [];
+    var rabbitsPosX = [], rabbitsPosY = [];
     
     canvas.height = (blockSize+blockSpacer)*fieldSize;
     canvas.width = (blockSize+blockSpacer)*fieldSize;
 
     document.onkeydown = function (event) {
         snake.shift(event.keyCode);
+        snake.eat();
+        rabbits.check();
+        //console.log(snakeLength);
     };
 
     //console.log(snakeCoordsX.join(', '));
@@ -29,76 +36,104 @@ window.onload = function() {
     var snake = new Snake();
     snake.start();
     snake.draw();
-
     
-    function Snake() {
-        var snakeCoordsX = [];
-        var snakeCoordsY = [];
+    var rabbits = new Rabbits();
+    rabbits.generate();
+    rabbits.draw();
 
-        //place snake on start position
+    function Snake() {
+
         this.start = function() {
-            for (var i=snakeLength-1; i>=0; i--) {
-                snakeCoordsX[i] = (blockSize+blockSpacer)*i;
+            for (var i=0; i<snakeLength; i++) {
+                snakeCoordsX[i] = (blockSize+blockSpacer)*(snakeLength-1)-((blockSize+blockSpacer)*i);
                 snakeCoordsY[i] = fieldSize/2*(blockSize+blockSpacer);
+            }
+            //console.log(snakeCoordsX.join(', '));
+        };
+        this.draw = function() {
+            for (var i=snakeCoordsX.length-1; i>=0; i--) {
+                if (i===0) {context.fillStyle="red";} else {context.fillStyle="black";}
+                context.fillRect(snakeCoordsX[i], snakeCoordsY[i], blockSize, blockSize);
             }
         };
         this.shift = function(direction) {
+            var moveTo;
             switch (direction) {
                 case 39: //right
-                    if (snakeCoordsX[3] != snakeCoordsX[4] + (blockSize + blockSpacer)) {
-                        eraseTail(snakeCoordsX[0], snakeCoordsY[0]);
-                        shiftSnakeCoords();
-                        snakeCoordsX[snakeCoordsX.length-1] += blockSize + blockSpacer;
-                        snake.draw();
-                    } else {
-                        //can't move inside yourself lol))
-                    }
+                    eraseTail();
+                    shiftSnakeCoords();
+                    snakeCoordsX[0] += blockSize + blockSpacer;
+                    snake.draw();
                     break;
                 case 37: //left
-                    if (snakeCoordsX[3] != snakeCoordsX[4] - (blockSize + blockSpacer)) {
-                        eraseTail(snakeCoordsX[0], snakeCoordsY[0]);
-                        shiftSnakeCoords();
-                        snakeCoordsX[4] -= blockSize + blockSpacer;
-                        snake.draw();
-                    } else {
-                        //can't move inside yourself lol))
-                    }
+                    eraseTail();
+                    shiftSnakeCoords();
+                    snakeCoordsX[0] -= blockSize + blockSpacer;
+                    snake.draw();
                     break;
                 case 40: //down
-                    if (snakeCoordsY[3] != snakeCoordsY[4] + (blockSize + blockSpacer)) {
-                        eraseTail(snakeCoordsX[0], snakeCoordsY[0]);
-                        shiftSnakeCoords();
-                        snakeCoordsY[4] += blockSize + blockSpacer;
-                        snake.draw();
-                    } else {
-                        //can't move inside yourself lol))
-                    }
+                    eraseTail();
+                    shiftSnakeCoords();
+                    snakeCoordsY[0] += blockSize + blockSpacer;
+                    snake.draw();
                     break;
                 case 38: //up
-                    if (snakeCoordsY[3] != snakeCoordsY[4] - (blockSize + blockSpacer)) {
-                        eraseTail(snakeCoordsX[0], snakeCoordsY[0]);
-                        shiftSnakeCoords();
-                        snakeCoordsY[4] -= blockSize + blockSpacer;
-                        snake.draw();
-                    } else {
-                        //can't move inside yourself lol))
-                    }
+                    eraseTail();
+                    shiftSnakeCoords();
+                    snakeCoordsY[0] -= blockSize + blockSpacer;
+                    snake.draw();
                     break;
             }
-            function eraseTail(x, y) {
+            function eraseTail() {
+                var x = snakeCoordsX[snakeCoordsX.length-1];
+                var y = snakeCoordsY[snakeCoordsY.length-1];
                 context.clearRect(x, y, blockSize, blockSize);
             }
             function shiftSnakeCoords() {
-                for (var i=0; i<snakeCoordsX.length-1; i++) {
-                    snakeCoordsX[i] = snakeCoordsX[i+1];
-                    snakeCoordsY[i] = snakeCoordsY[i+1];
+                for (var i=snakeCoordsX.length-1; i>0; i--) {
+                    snakeCoordsX[i] = snakeCoordsX[i-1];
+                    snakeCoordsY[i] = snakeCoordsY[i-1];
+                }
+                //console.log(snakeCoordsX.join(', '));
+            }
+        };
+        this.eat = function() {
+            for (var i=0; i<rabbitsPosX.length-1; i++) {
+                if (snakeCoordsX[0] === rabbitsPosX[i] && snakeCoordsY[0] === rabbitsPosY[i]) {
+                    //console.log('collide');
+                    snakeCoordsX.push(snakeCoordsX[snakeCoordsX.length-1]);
+                    snakeCoordsY.push(snakeCoordsY[snakeCoordsY.length-1]);
+                    rabbitsPosX.splice(i,1);
+                    rabbitsPosY.splice(i,1);
+                    rabbitsNumber--;
+                    break;
                 }
             }
         };
-        //draw snake
+    }
+    function Rabbits() {
+        this.generate = function() {
+            for (var i=0; i<rabbitsNumber; i++) {
+                while (true) {
+                    rabbitsPosX[i] = Math.floor(Math.random() * fieldSize) * (blockSize + blockSpacer);
+                    rabbitsPosY[i] = Math.floor(Math.random() * fieldSize) * (blockSize + blockSpacer);
+                    if (snakeCoordsX.indexOf(rabbitsPosX[i]) === -1 && snakeCoordsY.indexOf(rabbitsPosY[i]) === -1) {break;}
+                }
+            }
+            document.getElementById("rabbits_left").innerHTML = rabbitsNumber-1;
+        };
         this.draw = function() {
-            for (var i=0; i<snakeCoordsX.length; i++) {
-                context.fillRect(snakeCoordsX[i], snakeCoordsY[i], blockSize, blockSize);
+            for (var i=0; i<rabbitsPosX.length; i++) {
+                context.fillStyle="green";
+                context.fillRect(rabbitsPosX[i], rabbitsPosY[i], blockSize, blockSize);
+            }
+        };
+        this.check = function() {
+            if (rabbitsNumber-1 === 0) {
+                alert("You eat all rabbits! :)");
+                location.reload();
+            } else {
+                document.getElementById("rabbits_left").innerHTML = rabbitsNumber-1;
             }
         };
     }
